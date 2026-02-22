bbdfec47438173bbea6e7af47f38761d7cad74a4
jihoon <git40@gmail.com>
2026-02-22 23:26:25 +0900

    접근 불가 페이지

--- src/main/java/org/example/springauth/controller/ProductController.java
+++ src/main/java/org/example/springauth/controller/ProductController.java
@@ -4,7 +4,10 @@ package org.example.springauth.controller;
 
 
 import org.example.springauth.entity.User;
+import org.example.springauth.entity.UserRoleEnum;
 import org.example.springauth.security.UserDetailsImpl;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,17 @@ public class ProductController {
 //        Authentication 의 Principle
         User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
+
+        return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN) // 관리자용
+    @GetMapping("/products/secured")
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
